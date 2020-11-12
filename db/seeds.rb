3b437acf71bbd15dbc5e42f2f9bc4ebe80d78096
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2020-11-12 15:04:55 +0100

    compleated doses controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require "json"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cts_s = open(url).read
+cts = JSON.parse(cts_s)
+# cts.delete_at[0]
+
+cts["drinks"].each do |cocktail|
+  Ingredient.create(name: cocktail["strIngredient1"])
+end
